37979883a871717e6370c02c3ce05cac3b2c134e
Diego Duran <diegodurandaza6@gmail.com>
2021-05-11 22:12:58 -0500

    Configuración del servicio de json, configuración de consumo de API desde el App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,33 +8,45 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header/>
-        <Search/>
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+const App = () => {
+    const [videos, setVideos] = useState([]);
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+        .then(response => response.json())
+        .then(data => setVideos(data));
+    }, []);
 
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+    console.log(videos);
+
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
+            <Categories title="Mi lista">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+    
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+    
+            <Categories title="Originales de Platzi Video">
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+            <Footer/>
+        </div>
+    );
+}
 
 export default App;
